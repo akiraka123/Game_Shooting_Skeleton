53852d67b2166c60886b2066ef4461f4af3b704b
Akiraka <87097901+akiraka123@users.noreply.github.com>
2022-12-21 16:56:42 +0700

    FINAL

--- SFML_Projek/Game.cpp
+++ SFML_Projek/Game.cpp
@@ -6,7 +6,7 @@ void Game::initwindow()
 {
     this->window = new sf::RenderWindow(sf::VideoMode(1280, 720), " SUCCESS");
     this->window->setFramerateLimit(60);
-    this->window->setMouseCursorVisible(false);
+    this->window->setMouseCursorVisible(true);
 
 }
 
@@ -26,107 +26,210 @@ Game::~Game()
 void Game::Bullet()
 {
     this->bullet.setFillColor(sf::Color::Red);
-    this->bullet.setRadius(10.f);
+    this->bullet.setRadius(7.5f);
 
     this->Vbullet.push_back(sf::CircleShape(this->bullet));
-    
+
 }
 
 void Game::Enemy()
 {
-    this->enemy.setFillColor(sf::Color::Blue);
-    this->enemy.setSize(sf::Vector2f(50.f, 50.f));
+    this->enemytex.loadFromFile("TEXTURE/walk/go_1.png");
+    this->enemy.setTexture(this->enemytex);
+    this->enemy.scale(0.5f, 0.5f);
 
-    this->Venemies.push_back(sf::RectangleShape(this->enemy)); \
+
+    this->Venemies.push_back(sf::Sprite(this->enemy)); 
         this->enemyspawntimer = 0;
 }
 
 void Game::Player()
 {
-    this->player.setFillColor(sf::Color::White);
-    this->player.setRadius(50.f);
-    this->player.setPosition(0, this->window->getSize().y / 2 - this->player.getRadius());
+    this->playertex.loadFromFile("TEXTURE/1.png");
+    this->player.setTexture(this->playertex);
+    this->player.scale(1.4f, 1.4f);
+    this->player.setPosition(0, this->window->getSize().y / 2 - this->player.getLocalBounds().height);
+
     
+    //init
+    
+    if (this->fontscoreplayer.loadFromFile("FONT/Infinite Beyond Italic 400.ttf"))
+        {
+            std::cout << "font not found" << std::endl;
+        }
+    this->textscoreplayer.setFont(this->fontscoreplayer);
+    this->textscoreplayer.setCharacterSize(24);
+    this->textscoreplayer.setPosition(this->window->getSize().x / 2, 10.f);
+    
+    
+   /* this->temp = this->score;
+    this->ss << "SCORE PLAYER : " << this->temp;
+    this->textscoreplayer.setString(this->ss.str());*/
+    this->HP = 5;
+    this->score = -1;
     this->shoottimer = 0;
 
 }
 
 
-void Game::attackcolor() //shoot and position player
+void Game::attackcolor() //shoot and moveplayer
 {
-    this->placeforshoot = sf::Vector2f(this->player.getPosition().x + this->player.getRadius(), this->player.getPosition().y + this->player.getRadius());
+    this->placeforshoot = sf::Vector2f(this->player.getPosition().x + this->player.getGlobalBounds().width * 3 / 4, this->player.getPosition().y + this->player.getLocalBounds().height / 2 - 8.f);
     this->player.setPosition(this->player.getPosition().x, sf::Mouse::getPosition(*this->window).y);
 
-    if (this->shoottimer<15)
+    if (this->shoottimer < 15)
     {
         this->shoottimer++;
     }
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && this->shoottimer >= 15) {
-
-        this->bullet.setPosition(this->placeforshoot); 
+        //bullet
+        this->bullet.setPosition(this->placeforshoot);
         this->Vbullet.push_back(sf::CircleShape(this->bullet));
-        
+
         this->shoottimer = 0;
     }
-
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        //sound effect bullet
+        this->buffsoundBullet.loadFromFile("Music/sound/pistol1.ogg");
+        this->soundShoot.setBuffer(this->buffsoundBullet);
+        this->soundShoot.play();
+    }
     for (size_t i = 0; i < this->Vbullet.size(); i++)
     {
-        this->Vbullet[i].move(10.f,0.f);
+        this->Vbullet[i].move(10.f, 0.f);
 
         if (this->Vbullet[i].getPosition().x > this->window->getSize().x)
         {
-            this->Vbullet.erase(this->Vbullet.begin()+i);
+            this->Vbullet.erase(this->Vbullet.begin() + i);
         }
 
     }
 }
 
-void Game::Enemies() 
+void Game::Enemies()
 {
     //ENEMIES
-		if(this->enemyspawntimer < 20)
-			this->enemyspawntimer++;
+    if (this->enemyspawntimer < 45)
+        this->enemyspawntimer++;
 
-		if (this->enemyspawntimer >= 20)
-		{
-			this->enemy.setPosition(this->window->getSize().x, (rand() % int(this->window->getSize().y - this->enemy.getSize().y)));
-			this->Venemies.push_back(sf::RectangleShape(this->enemy));
+    if (this->enemyspawntimer >= 45)
+    {
+        this->enemy.setPosition(this->window->getSize().x, (rand() % int(this->window->getSize().y - this->enemy.getGlobalBounds().height)));
+        this->Venemies.push_back(sf::Sprite(this->enemy));
 
-			this->enemyspawntimer = 0;
-		}
+        this->enemyspawntimer = 0;
+    }
 
-		for (size_t i = 0; i < this->Venemies.size(); i++)
-		{
-			this->Venemies[i].move(-5.f, 0.f);
+    for (size_t i = 0; i < this->Venemies.size(); i++)
+    {
+        this->Venemies[i].move(-5.f, 0.f);
 
-			if (this->Venemies[i].getPosition().x > this->window->getSize().x)
-				this->Venemies.erase(this->Venemies.begin() + i);
-		}
+        if (this->Venemies[i].getPosition().x > this->window->getSize().x)
+            this->Venemies.erase(this->Venemies.begin() + i);
+    }
 
-		
+
 
 }
 
 void Game::collision()
 {
     //Collision bullet to enemy
-		if (!this->Venemies.empty() && !this->Vbullet.empty())
-		{
-			for (size_t i = 0; i < this->Vbullet.size(); i++)
-			{
-				for (size_t k = 0; k < this->Venemies.size(); k++)
-				{
-					if (this->Vbullet[i].getGlobalBounds().intersects(this->Venemies [k].getGlobalBounds()))
-					{
-						this->Vbullet.erase(this->Vbullet.begin() + i);
-						this->Venemies.erase(this->Venemies.begin() + k);
-						break;
-					}
-				}
-			}
-		}
+    if (!this->Venemies.empty() && !this->Vbullet.empty())
+    {
+        for (size_t i = 0; i < this->Vbullet.size(); i++)
+        {
+            for (size_t k = 0; k < this->Venemies.size(); k++)
+            {
+                if (this->Vbullet[i].getGlobalBounds().intersects(this->Venemies[k].getGlobalBounds()))
+                {
+                    this->buffdet.loadFromFile("Music/sound/1death.ogg");
+                    this->sounddet.setBuffer(this->buffdet);
+                    this->sounddet.play();
+                    
+                    this->Vbullet.erase(this->Vbullet.begin() + i);
+                    this->Venemies.erase(this->Venemies.begin() + k);
+                    this->score++;
+                    break;
+                }
+            }
+        }
+    }
 
+    //collision player 
+            // collision top
+    if (this->player.getPosition().y < 0.f)
+    {
+        this->player.setPosition(this->player.getPosition().x, 0.f);
+    }
+            // collision bot
+    if (this->player.getPosition().y + this->player.getGlobalBounds().height > 720.f)
+    {
+        this->player.setPosition(this->player.getPosition().x, 720.f - this->player.getGlobalBounds().height);
+    }
+            //collision enemy and player
+    if (!this->Venemies.empty()) {
+        for (size_t i = 0; i < this->Venemies.size(); i++)
+        {
+            if (this->player.getGlobalBounds().intersects(this->Venemies[i].getGlobalBounds()))
+            {
+                
+                this->HP-=1;
+                this->Venemies.erase(this->Venemies.begin() + i);
+                break;
+            }
+        }
+    }
+}
 
+void Game::Background()
+{
+    if (!this->backgroundtex.loadFromFile("TEXTURE/background.jpg"))
+    {
+       std::cout << "error background"<<std::endl;
+    }
+    this->Sbackground.setTexture(this->backgroundtex);
+    this->Sbackground.scale(0.5f, 0.5f);
+}
+
+void Game::Music() {
+
+    if (!this->music.openFromFile("Music/bgm.ogg"))
+    {
+        std::cout << "error music" << std::endl;
+    }
+    
+    music.play();
+}
+
+void Game::Score()
+{
+    std::stringstream ss;
+    int temp = this->score;
+    ss << "SCORE PLAYER : " << temp;
+    this->textscoreplayer.setString(ss.str());
+}
+
+void Game::gameover()
+{
+    this->textGameOVer.setFont(this->fontscoreplayer);
+    this->textGameOVer.setCharacterSize(24);
+    this->textGameOVer.setPosition(this->window->getSize().x / 2, this->window->getSize().y / 2);
+    this->textGameOVer.setString("GAME OVERRRRR UIIII");
+    
+    while (this->window->pollEvent(this->sfevent))
+    {
+        if (this->sfevent.type == sf::Event::Closed)
+            this->window->close();
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            this->window->close();
+        }
+       
+    }
+
+    
 }
 
 void Game::updateSFMLEvents()
@@ -136,25 +239,37 @@ void Game::updateSFMLEvents()
     {
         if (this->sfevent.type == sf::Event::Closed)
             this->window->close();
-
+        
     }
 
+    this->Score();
     this->attackcolor();
     this->Enemies();
     this->collision();
+
 }
 
 void Game::update()
 {
     
-    this->updateSFMLEvents();
+    if (this->HP <= 0)
+    {
+        this->gameover();
+        
+    }
+    else { 
+
+        this->updateSFMLEvents();
+        
     
+    }
 }
 
 void Game::render()
 {
-
-    this->window->clear(sf::Color::Black);
+    this->window->clear();
+    this->window->draw(this->Sbackground);
+    
     this->window->draw(this->player);
 
     for (size_t i = 0; i < this->Venemies.size(); i++)
@@ -165,18 +280,21 @@ void Game::render()
     {
         this->window->draw(this->Vbullet[i]);
     }
-
-    this->window->display();    
+    this->window->draw(this->textscoreplayer);
+    this->window->draw(this->textGameOVer);
+    this->window->display();
 
 }
 
 
+
 void Game::run()
 {
+    this->Background();
+    this->Music();
     this->Bullet();
     this->Enemy();
     this->Player();
-
     while (this->window->isOpen())
     {
 
@@ -184,6 +302,3 @@ void Game::run()
         this->render();
     }
 }
-
-
-
--- SFML_Projek/Game.h
+++ SFML_Projek/Game.h
@@ -4,66 +4,89 @@
 #include <ctime>
 #include <cstdlib>
 #include <vector>
-
+#include <sstream>
 #include"SFML/Audio.hpp"
 #include"SFML/Network.hpp"
 #include"SFML/System.hpp"
 #include"SFML/Window.hpp"
 #include"SFML/Graphics.hpp"
 
-
+using namespace sf;
 
 class Game
 {
 
 private:
+
 	
-	//var
-	sf::RenderWindow* window;
-	sf::Event sfevent;
+	RenderWindow* window;
+	Event sfevent;
+	Sprite Sbackground;
+	Texture backgroundtex;
+	Music music;
+
 
 	//player
-	sf::CircleShape player;
-	sf::Vector2f placeforshoot;
+	Sprite player;
+	Texture playertex;
+	Vector2f placeforshoot;
+	Font fontscoreplayer;
+	Text textscoreplayer;
+	int HP;
+	int score;
 	int shoottimer;
-	int enemyspawntimer;
 
 	//enemy
-	sf::RectangleShape enemy;
-	std::vector <sf::RectangleShape> Venemies;
-
-	//bullet
-	sf::CircleShape bullet;
-	std::vector <sf::CircleShape> Vbullet;
+	Sprite enemy;
+	Texture enemytex;
+	SoundBuffer buffdet;
+	Sound sounddet;
 	
+	std::vector <Sprite> Venemies; 
+	int enemyspawntimer;
+	
+	//bullet
+	CircleShape bullet;
+	std::vector <CircleShape> Vbullet;
+	SoundBuffer buffsoundBullet;
+	Sound soundShoot;
+
 
 	//init
 	void initwindow();
 
+	//gameover
+	Text textGameOVer;
+
 
 public:
-	
+
 	//construtors / destructors
 	Game();
 	virtual ~Game();
-	
+
+	//UI
+	void Background();
+	void Music();
+
 	//character ingame
 	void Player();
 	void Bullet();
 	void Enemy();
-	
 
-		//function
-		
+
+	//function
+
 	void attackcolor();
-	void Enemies(); 
+	void Enemies();
 	void collision();
+	void Score();
 
 	void updateSFMLEvents();
 	void update();
 	void render();
 	void run();
-
+	void gameover();
 
 
 };
